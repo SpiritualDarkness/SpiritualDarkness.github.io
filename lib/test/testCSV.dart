d25b61526652c2ed08cd0978943baeba60de8ca3
Megumin <megumin_lyq@outlook.com>
2023-11-17 12:24:06 +0800

    refactor build

--- lib/test/testCSV.dart
+++ lib/test/testCSV.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart' show rootBundle;
 
 class testwidget extends StatelessWidget {
   Future<String> loadAsset() async {
-    return await rootBundle.loadString('./resource/book_list/tmp.csv');
+    return await rootBundle.loadString('resource/book_list/tmp.csv');
   }
 
   @override
